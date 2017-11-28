e2e050dd777fa37ea5bff50cb304378062a1a8ff
dwakeherrera <deivid.wgh@gmail.com>
2017-11-28 08:56:30 +0100

    Añadido CGtk (introducción a Gtk)

--- JVector/src/org/institutoserpis/ed/Vector.java
+++ JVector/src/org/institutoserpis/ed/Vector.java
@@ -12,14 +12,79 @@ public class Vector {
 
 		int position = index0f(v, value);
 		System.out.println("position=" + position);
+		int min = min(v);
+		System.out.println("min=" + min);
+
+		int minPosition = index0fMin(v);
+		System.out.println("min position: " + minPosition);
+
+		selectionSort(v);
 	}
 
 	public static int index0f(int[] v, int value) {
+//		int index = 0;
+//		while (index < v.length && v[index] != value) //0j0 con el orden de la condición
+//			index++;
+//		if (index == v.length)
+//			return -1;
+//		return index;
+//		---------método wirtz---------
+//		for (int index = 0; index < v.length; index++)
+//			if (v[index] == value)
+//				return index;
 		int index = 0;
-		while (index < v.length && v[index] != value) //0j0 con el orden de la condición
+		while (index < v.length && v[index] != value)
 			index++;
-		if (index == v.length)
-			return -1;
-		return index;
+		return index < v.length ? index : -1;
+	}
+
+	public static int min(int[] v) {
+		int min = v[0];
+//		for (int index = 1; index < v.length; index += 1)
+//			if (min > v[index])
+//				min = v[index];
+//		return min;
+		for (int item : v)
+			if (item < min)
+				min = item;
+		return min; //nuevo for
+	}
+
+	public static int sum(int[] v) {
+		int suma = 0;
+
+//		for (int index = 0; index < v.length; index++)
+//			suma = suma + v[index];
+		for (int item : v)
+			suma = suma + item;
+		return suma;
+	}
+
+	public static int index0fMin(int[] v) {
+		int index0fMin = 0;
+
+		for (int index = 0; index < v.length; index++)
+			if (v[index] < v[index0fMin])
+				index0fMin = index;
+		return index0fMin;
+	}
+
+	public static void selectionSort(int[] v) {
+		int selectedIndex, index0fmin, aux;
+		int n = v.length;
+
+		for (int index = 0; index < n; index++) {
+			index0fmin = index;
+				for (selectedIndex = index + 1; selectedIndex < n; selectedIndex++)
+					if (v[selectedIndex] < v[index0fmin])
+	                	  index0fmin = selectedIndex;
+				if (index0fmin != index) {
+	            	aux = v[index];
+	            	v[index] = v[index0fmin];
+	            	v[index0fmin] = aux;
+	            }
+	      }
+	      for (int index = 0; index < n; index++)
+	    	  System.out.print(v[index] + " ");
 	}
 }
---
+++ JVector/src/org/institutoserpis/ed/VectorTest.java
@@ -0,0 +1,50 @@
+package org.institutoserpis.ed;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VectorTest {
+
+	@Test
+	public void index0f() {
+		int[] v = new int[] {9, 15, 7, 12, 6};
+		assertEquals(0, Vector.index0f(v, 9));
+		assertEquals(2, Vector.index0f(v, 7));
+		assertEquals(4, Vector.index0f(v, 6));
+		assertEquals(-1, Vector.index0f(v, 33));
+	}
+
+	@Test
+	public void index0fEmpty() {
+		int[] v = new int[] {};
+		assertEquals(-1, Vector.index0f(v, 7));
+	}
+	
+	@Test
+	public void min() {
+		assertEquals(6, Vector.min(new int[] {9, 15, 7, 12, 6}));
+		assertEquals(7, Vector.min(new int[] {9, 15, 7, 12, 16}));
+		assertEquals(9, Vector.min(new int[] {9, 15, 17, 12, 16}));
+	}
+
+	@Test
+	public void index0fMin() {
+		assertEquals(4, Vector.index0fMin(new int[] {9, 15, 7, 12, 6}));
+		assertEquals(0, Vector.index0fMin(new int[] {9, 15, 17, 12, 16}));
+	}
+
+	@Test
+	public void selectionSort() {
+		int[] v = new int[] {9,15,7,12,6};
+		Vector.selectionSort(v);
+		assertArrayEquals(new int[] {6,7,9,12,15}, v);
+	}
+
+	@Test
+	public void sum() {
+		assertEquals(49, Vector.sum(new int[] {9, 15, 7, 12, 6}));
+		assertEquals(0, Vector.sum(new int[] {}));
+	}
+
+}
