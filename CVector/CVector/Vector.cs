1f32a35f55f50e20d98bc242e27b97a3aadd0c11
dwakeherrera <deivid.wgh@gmail.com>
2018-03-02 09:24:34 +0100

    Añadido ASuma sacado de internet y ASumav2 siguiendo la clase

--- CVector/CVector/Vector.cs
+++ CVector/CVector/Vector.cs
@@ -7,6 +7,13 @@ namespace CVector
 		public static void Sort(int[] v) {
 			
 		}
+
+		public static void IndexMin(int[] v, int initialIndex) {
+			return -1;
+		}
+
+		public static void Swap(int[] v, int index, int otherIndex) {
+		}
 	}
 }
 
--- CVector/CVector/VectorTest.cs
+++ CVector/CVector/VectorTest.cs
@@ -15,6 +15,12 @@ namespace CVector
 			Vector.Sort(v);
 			CollectionAssert.AreEqual (new int[] {7, 9, 12, 13, 15}, v);
 		}
+
+		[Test]
+		public void IndexMin() {
+			int[] v = {12, 9, 15, 7, 13};
+			int indexMin = Vector.IndexMin (v, 4);
+			Assert.AreEqual (4, indexMin);
+		}
 	}
 }
-
